2f83f1df545b786e896ba5d7b862aa2b3bb4ede2
Luciano Oliveira <luciano101@msn.com>
2017-08-11 11:42:59 -0700

    fix issues with values not being pushed to array with .map method from jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,59 @@
 $(document).ready(init);
+var answersTotal = [];
+var answers;
+var answers1;
+var answer;
 
 function init(){
 	$('select').material_select();
-	// $('.btn').on('click', grabAnswer);
+	$('#nextBtn').on('click', grabAnswer);
+	// $('#nextBtn').on('click', grabAnswer);
 }
 
+// function grabAnswer(){
+// 	var questions = ["1"];
+// 	answer1 = questions.forEach(function(q){
+// 		// answers = $(".select" +  q + " option:selected").val();
+// 		console.log('answer from for each method: ', answers);
+// 		var testanswer = answersTotal.push(answers1);
+// 		console.log('testanswer: ', testanswer);
+// 	});
+// 	console.log('foreach method: ', answer1);
+// 	console.log('answers: ', answers);
+// 	console.log(answersTotal);
+// }
+
 function grabAnswer(){
-	var questions = ["1", "2", "3", "4", "5"];
-	questions.forEach(function(q){
-		var answers = $(".select" +  q + " option:selected").val();
+	$('#nextBtn').on('click', function(){
+		answers = $("select option:selected");
+
+		var values = $.map(answers ,function(option) {
+    	return option.value;
+		});
+		console.log('values: ', values);
+
+		answersTotal.push(answers);
+		console.log(answersTotal);
+		$('.current').removeClass('current').hide().next().show().addClass('current');
+		if ($('.current').hasClass('last')){
+			$('#nextBtn').hide();
+			$('#submitBtn').show();
+		};
 	});
 }
 
-function nextQuestion(){
-	grabAnswer();
-	$('.btn').on('click', function(){
-		$('.current').removeClass('current').hide().next().show().addClass('current');
-	})
-}
 
-$('.btnSubmit').click(function(e){
-	e.preventDefault();
-	console.log('submitted:');
-});
+
+
+// var options = $('#selectBox option');
+
+// var values = $.map(options ,function(option) {
+//     return option.value;
+// });
+
+
+// $('.btnSubmit').click(function(e){
+// 	e.preventDefault();
+// 	console.log('submitted:');
+// });
      
